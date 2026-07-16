6258fd8683016ee6e9e8d8a154c78eb6530c2ca1
Mukesh Kumar <mukeshkumarsingh7488@gmail.com>
2026-07-16 21:07:46 +0530

    feat: optimize PWA manifest and production metadata

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -30,17 +30,11 @@ export default function Footer() {
               <a href="https://t.me/+hBAT4kWo63A4ZWY1" target="_blank" rel="noopener noreferrer">
                 <i className="fa-brands fa-telegram"></i>
               </a>
-              <a href="https://chat.whatsapp.com/B4t82SWBcgOIZTeQXp1wDI" target="_blank" rel="noopener noreferrer">
-                <i className="fa-brands fa-whatsapp"></i>
-              </a>
               <a href="https://x.com/MukeshKuma48159" target="_blank" rel="noopener noreferrer">
                 <i className="fa-brands fa-x-twitter"></i>
               </a>
               <a href="https://www.linkedin.com/in/mukeshraj-br30/" target="_blank" rel="noopener noreferrer">
                 <i className="fa-brands fa-linkedin"></i>
-              </a>
-              <a href="https://www.threads.com/@br30traderofficial" target="_blank" rel="noopener noreferrer">
-                <i className="fa-brands fa-threads"></i>
               </a>
             </div>
           </div>
